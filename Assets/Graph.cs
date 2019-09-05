e1c90ddea461ca8bd56bb52c9e36c87d26131725
bk62 <bkafle862@gmail.com>
2019-09-05 22:47:59 +0545

    Domain is rectangular grid of points on the x-z plane (as opposed to the square grid in the tutorial)

--- Assets/Graph.cs
+++ Assets/Graph.cs
@@ -2,11 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Graph : MonoBehaviour
-{
-    // resolution -- num instantiated prefabs
-    [Range(10, 100)]
-    public int resolution = 10;
+public class Graph : MonoBehaviour {
+
+    const float pi = Mathf.PI;
+
+    [Range (10, 100)]
+    public int resolutionX = 10;
+    [Range (10, 100)]
+    public int resolutionZ = 1;
+
     // prefab representing graph points
     public Transform pointPrefab;
 
@@ -14,60 +18,67 @@ public class Graph : MonoBehaviour
 
     public GraphFunctionName function;
     static GraphFunction[] functions = {
-        SineFunction, MultiSineFunction
+        SineFunction,
+        MultiSineFunction
     };
 
-    void Awake() {
+    void Awake () {
         // fit resolution objects within 2f units
-        float step = 2f / resolution;
+        float stepX = 2f / resolutionX;
+        float stepZ = 2f / resolutionZ;
         // scale by same factor so resoltion prefabs fit from -1 to 1
-        Vector3 scale = Vector3.one * step;
+        Vector3 scaleX = Vector3.one * stepX;
+        Vector3 scaleZ = Vector3.one * stepZ;
+
         Vector3 position = Vector3.zero;
 
-        points = new Transform[resolution];
-        for (int i = 0; i < points.Length; i++) {
-            Transform point = Instantiate(pointPrefab);
-            // shift half a cube to the right, scale spacing and center at 0
-            position.x = (i + 0.5f) * step - 1f;
-            // position.y = position.x; // y = x
-            // position.y = position.x * position.x;// y = x^2
-            point.localPosition = position;
-            point.localScale = scale;
-            // org hierarchy
-            // don't keep at original pos, rot and scale
-            point.SetParent(transform, false);
+        points = new Transform[resolutionX * resolutionZ];
+        for (int i = 0, z = 0; z < resolutionZ; z++) {
+            position.z = (z + 0.5f) * stepZ - 1f;
+            for (int x = 0; x < resolutionX; x++, i++) {
+                Transform point = Instantiate (pointPrefab);
+                // shift half a cube to the right, scale spacing and center at 0
+                position.x = (x + 0.5f) * stepX - 1f;
+                
+                point.localPosition = position;
+                // TODO
+                point.localScale = resolutionX > resolutionZ ? scaleX : scaleZ; 
+                // point.localScale.z = scaleZ;
+                // org hierarchy
+                // don't keep at original pos, rot and scale
+                point.SetParent (transform, false);
 
-            points[i] = point;
+                points[i] = point;
+            }
         }
     }
 
-    void Update() {
+    void Update () {
         float t = Time.time;
-        GraphFunction f = functions[(int)function];
-        for(int i = 0; i < points.Length; i++) {
+        GraphFunction f = functions[(int) function];
+        for (int i = 0; i < points.Length; i++) {
             Transform point = points[i];
             Vector3 position = point.localPosition;
-            Vector2 xz = new Vector2(position.x, position.y);
-            position.y = f(xz, t);
+            Vector2 xz = new Vector2 (position.x, position.y);
+            position.y = f (xz, t);
             point.localPosition = position;
         }
     }
 
-    static float SineFunction(Vector2 xz, float t) {
+    static float SineFunction (Vector2 xz, float t) {
         float x = xz.x;
-        return Mathf.Sin(Mathf.PI * (x + t));
+        return Mathf.Sin (pi * (x + t));
     }
 
-    static float MultiSineFunction(Vector2 xz, float t) {
+    static float MultiSineFunction (Vector2 xz, float t) {
         float x = xz.x;
-        float y = Mathf.Sin(Mathf.PI * (x + t));
+        float y = Mathf.Sin (pi * (x + t));
         // add another sine wave with half the freq
         // and amplitude
-        y += Mathf.Sin(2f * Mathf.PI * (x + t)) / 2f;
+        y += Mathf.Sin (2f * pi * (x + t)) / 2f;
         // amplitude of resulting function is 3/2
         // normalize
         y *= 2f / 3f;
         return y;
     }
 }
-
